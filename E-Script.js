ab4e70fb3fed7d3e7306ca2c6a80fa10f996c838
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-28 09:37:02 -0300

    Add files via upload

---
+++ E-Script.js
@@ -0,0 +1,20 @@
+let idade = 20;
+
+if (idade == 20) {
+    idade = idade + 15;
+        console.log("idade adicionada com sucesso");
+        console.log (idade);
+}
+
+/*No caso, mesmo que "20" seja uma string essa atribuição será aceita
+
+Mas neste caso não, pois o acréscimo de mais um igual deixa mais restritivo
+let idade = 20;
+
+if (idade === 20) {
+    console.log ("Você tem 20 anos");
+}
+*/
+
+
+
